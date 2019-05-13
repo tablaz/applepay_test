3cc317167b75471d2e5b36b1fa12d46512972bd1
Ricardo Restrepo M <ricardo-julian.restrepo@leroymerlin.es>
2019-05-13 14:30:51 +0200

    Arreglos

--- applepay_test/ViewController.swift
+++ applepay_test/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PassKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK - Properties
+    private var paymentRequest: PKPaymentRequest = {
+        let request = PKPaymentRequest()
+        request.merchantIdentifier = "merchant.leroymerlin.app.retail"
+        request.supportedNetworks = [.visa, .masterCard]
+        request.supportedCountries = ["ES","US"]
+        request.merchantCapabilities = .capability3DS
+        request.countryCode = "ES"
+        request.currencyCode = "EUR"
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "iPhone Xs 64 Gb", amount: 99.99)]
+        return request
+    }()
+    
+    @IBAction func purchaseItem(_ sender: Any) {
+        if let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest) {
+            controller.delegate = self
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
 }
-
+extension ViewController: PKPaymentAuthorizationViewControllerDelegate {
+    
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+}
